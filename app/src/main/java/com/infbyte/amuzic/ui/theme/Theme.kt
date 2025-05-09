56619e2df9cb0ad5cf99922437d5c38a82a4feef
Shubert Munthali <shubertm.m@gmail.com>
2025-05-09 17:25:36 +0200

    Use proper API for edge to edge (#37)

--- app/src/main/java/com/infbyte/amuzic/ui/theme/Theme.kt
+++ app/src/main/java/com/infbyte/amuzic/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.infbyte.amuzic.ui.theme
 
-import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Shapes
@@ -8,11 +7,6 @@ import androidx.compose.material3.Typography
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
 
 private val lightScheme =
     lightColorScheme(
@@ -103,20 +97,6 @@ fun AmuzicTheme(
         } else {
             lightScheme
         }
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            val activity = (view.context as Activity)
-            val window = activity.window
-            WindowCompat.setDecorFitsSystemWindows(window, false)
-            window.statusBarColor = Color.Transparent.toArgb()
-            window.navigationBarColor = Color.Transparent.toArgb()
-            WindowCompat.getInsetsController(window, view).run {
-                isAppearanceLightStatusBars = !darkTheme
-                isAppearanceLightNavigationBars = !darkTheme
-            }
-        }
-    }
 
     MaterialTheme(
         colorScheme = colors,
