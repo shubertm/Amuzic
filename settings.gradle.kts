7646ec9ead6ba73c1eb5e9a20449712f06185133
Shubert Munthali <shubertm.m@gmail.com>
2025-06-16 20:05:48 +0200

    Minor refactors

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,14 +4,8 @@ pluginManagement {
         google()
         mavenCentral()
     }
-    plugins {
-        id("org.jetbrains.kotlin.jvm") version
-                "2.1.20"
-    }
 }
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
-}
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
